f016bb3167d00bdc86e77b0ed10f44843564dcf1
Grzegorz Strączek <grzegorz.straczek@neti-soft.com>
2024-06-28 12:15:05 +0200

    fix(report): change conn string

--- src/db.ts
+++ src/db.ts
@@ -1,7 +1,8 @@
 import { Pool } from 'pg';
 
 import { env } from './common/utils/envConfig';
-const connectionString = `postgresql://${env.DB_USER}:${env.DB_PASSWORD}@${env.DB_HOST}:${env.DB_PORT}/${env.DB_NAME}?sslmode=${env.isDev ? 'disable' : 'require'}`;
+
+const connectionString = env.DB_URL;
 export const db = new Pool({
   connectionString,
 });
