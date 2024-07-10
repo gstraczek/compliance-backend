7385877e1976864113cd2c241419be4455964064
Grzegorz Strączek <grzegorz.straczek@neti-soft.com>
2024-07-10 13:10:20 +0200

    fix(report): fix build cleanup hardcoded

--- src/common/middleware/webhooksHandler.ts
+++ src/common/middleware/webhooksHandler.ts
@@ -5,6 +5,7 @@ import { logger } from '@/server';
 
 import createComment from '../utils/createGhComment';
 import { env } from '../utils/envConfig';
+import { getAddressFromIssue } from '../utils/getAddressFromIssue';
 
 const webhooks = new Webhooks({
   secret: env.WEBHOOK_SECRET,
@@ -16,8 +17,7 @@ webhooks.on(['issue_comment.created', 'issue_comment.edited'], async (context) =
       const issue = context.payload.issue.body;
 
       if (issue?.includes('Organization On-chain Identity')) {
-        // const allocatorAddress = getAddressFromIssue(issue);
-        const allocatorAddress = 'f2arpo76vxmneuor5qunbpybm24k7efomlev3rk5i';
+        const allocatorAddress = getAddressFromIssue(issue);
 
         if (allocatorAddress) {
           const owner = context.payload.repository.owner.login;
--- src/common/utils/createGhComment.ts
+++ src/common/utils/createGhComment.ts
@@ -1,12 +1,18 @@
-import { Octokit } from '@octokit/rest';
-
 import { env } from './envConfig';
 
-const createComment = async (owner: string, repo: string, issue_number: number, body: string) => {
-  const octokit = new Octokit({
+let octokit: any;
+
+const initializeOctokit = async () => {
+  const { Octokit } = await import('@octokit/rest');
+  octokit = new Octokit({
     auth: env.GITHUB_TOKEN,
   });
+};
 
+const createComment = async (owner: string, repo: string, issue_number: number, body: string) => {
+  if (!octokit) {
+    await initializeOctokit();
+  }
   await octokit.issues.createComment({
     owner,
     repo,
