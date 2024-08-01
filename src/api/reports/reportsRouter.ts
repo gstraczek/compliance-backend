f80955c32f6a9cb57e046d63fc64509033ce0ba6
Kacper Żuk <kacper.zuk@neti-soft.com>
2024-08-01 18:35:21 +0200

    Storing reports in DB

---
+++ src/api/reports/reportsRouter.ts
@@ -0,0 +1,31 @@
+import { OpenAPIRegistry } from '@asteasolutions/zod-to-openapi';
+import express, { Request, Response, Router } from 'express';
+import { z } from 'zod';
+
+import { createApiResponse } from '@/api-docs/openAPIResponseBuilders';
+import { handleServiceResponse } from '@/common/utils/httpHandlers';
+
+import { ReportsSchema } from './reportsModel';
+import { reportsService } from './reportsService';
+
+export const reportsRegistry = new OpenAPIRegistry();
+
+reportsRegistry.register('Reports', ReportsSchema);
+
+export const ReportsRouter: Router = (() => {
+  const router = express.Router();
+
+  reportsRegistry.registerPath({
+    method: 'get',
+    path: '/reports',
+    tags: ['Reports'],
+    responses: createApiResponse(z.array(ReportsSchema), 'Success'),
+  });
+
+  router.post('/', async (_req: Request, res: Response) => {
+    const reports = await reportsService.listReports();
+    handleServiceResponse(reports, res);
+  });
+
+  return router;
+})();
--- src/common/utils/dbQuery.ts
+++ src/common/utils/dbQuery.ts
@@ -30,3 +30,6 @@ export const providerDistributionQuery = `
   ORDER BY total_deal_size DESC`;
 
 export const generatedReportQuery = `SELECT * FROM generated_reports WHERE client_address_id = $1 ORDER BY created_at DESC LIMIT 1`;
+
+export const storeReportQuery = `insert into allocator_generated_reports (address, address_id, name, file_path) values ($1, $2, $3, $4)`;
+export const listReportsQuery = `select distinct on (address) address, address_id, name, file_path, created_at from allocator_generated_reports order by created_at desc`;
