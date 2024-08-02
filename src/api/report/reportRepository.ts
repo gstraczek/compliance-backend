457c2451c5a150de0ed6fde5616c9dd2b8d6fab9
Kacper Żuk <kacper.zuk@neti-soft.com>
2024-08-02 16:41:48 +0200

    Fix connection leak

--- src/api/report/reportRepository.ts
+++ src/api/report/reportRepository.ts
@@ -464,8 +464,6 @@ export const reportRepository = {
   getClientsDeals: async (verifierClientsData: ClientsByVerifier[]): Promise<ClientsDeals[]> => {
     const clientAddressIds = verifierClientsData.map((e) => e.addressId);
     try {
-      db.connect();
-
       const values = [clientAddressIds];
 
       const result = await db.query(clientDealsQuery, values);
